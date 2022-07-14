1249e708742f3cffc487a16c252732a0d6c9c5a8
Nesia <helena.maslowska17@gmail.com>
2022-07-14 11:27:50 +0200

    Add open file

--- image_editor.py
+++ image_editor.py
@@ -1,18 +1,54 @@
 from PIL import Image
 import sys
-from PyQt5.QtWidgets import * 
-from PyQt5.QtGui import * 
-from PyQt5.QtCore import * 
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide2.QtGui import QPixmap
+from PySide2.QtCore import QFile 
 import os
 import subprocess
+from mainwindow import Ui_MainWindow
 FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
 
 width = 1000
 height = 800
 
-class Window(QMainWindow):
+class MainWindow(QMainWindow):
+	def __init__(self) -> None:
+		super(MainWindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.set_functions()
+		
+
+	def set_functions(self):
+		self.ui.add_img_btn.clicked.connect(self.clickme)
+
+	def clickme(self):
+		filename, filter = QFileDialog.getOpenFileName(
+            parent=self, caption='Open file', filter="Image files (*.png *.jpg)")
+		pixmap = QPixmap(filename)
+		if (pixmap.width() >= 700):
+			pixmap = pixmap.scaledToWidth(700)
+		if (pixmap.height() >= 600):
+			pixmap = pixmap.scaledToHeight(600)
+		print(pixmap.width(), pixmap.height())
+		#pixmap5 = pixmap.scaled(64, 64)
+		self.ui.image_shower.setPixmap(pixmap)
+		self.ui.image_shower.repaint()
+	
+
+
+'''class Window(QMainWindow):
+
 	def __init__(self):
 		super().__init__()
+		name_list = ["name", "s_width", "s_height", "b_width", "b_height", "function"]
+		set = {zip(name_list, set(enumerate(name_list)))}
+		
+		set_buttons = [
+			[	"Add image", 	20, 	int(height / 2) - 20, 	100, 	40,		self.clickme],
+			[	"Save",			20, 	int(height / 2) + 30,	100,	40 	]
+		]
+
 		self.setWindowTitle("Paintroom")
 		self.setGeometry(400, 200, width, height)
 		self.label = QLabel(self)
@@ -21,10 +57,18 @@ class Window(QMainWindow):
 		self.show()
 
 	def UiComponents(self):
-		button = QPushButton("Add image", self)
-		button.setGeometry(20, height / 2 - 20, 100, 40)
-		button.clicked.connect(self.clickme)
-	
+		# for button in self.set_buttons:
+		# 	button = QPushButton(button[0], self)
+		# 	button.setGeometry(button[1], button[2], button[3], button[4])
+		# 	button.clicked.connect(self.clickme)
+		for i in self.set_buttons:
+			button = QPushButton(i[self.set["name"]], self)
+			button.setGeometry(i[self.set["s_width"]], i[self.set["s_height"]], i[self.set["b_width"]], i[self.set["b_height"]]  )
+			button.clicked.connect(self.clickme)
+		# button = QPushButton("Add image", self)
+		# button.setGeometry(20, int(height / 2) + 40, 100, 40  )
+		# button.clicked.connect(self.clickme)
+
 	def explore(path):
 		# explorer would choke on forward slashes
 		path = os.path.normpath(path)
@@ -35,19 +79,21 @@ class Window(QMainWindow):
 			subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(path)])
 
 	def clickme(self):
-		explore()
-		self.pixmap = QPixmap('bird.jpg')
+		filename, filter = QFileDialog.getOpenFileName(
+            parent=self, caption='Open file', filter='*.png')
+		self.pixmap = QPixmap(filename)
 		self.label.setPixmap(self.pixmap)
 		self.label.resize(self.pixmap.width(), self.pixmap.height())
-		print("pressed")
+		print("pressed")'''
 
-App = QApplication(sys.argv)
 
-# create the instance of our Window
-window = Window()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-# start the app
-sys.exit(App.exec())
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
 
 # def text_setup(window):
 # 	helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
---
+++ mainwindow.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1001, 691)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(10, 10, 231, 621))
+        self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.Settings = QToolBox(self.verticalLayoutWidget_2)
+        self.Settings.setObjectName(u"Settings")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.page.setGeometry(QRect(0, 0, 229, 557))
+        self.verticalLayoutWidget = QWidget(self.page)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(50, 10, 121, 151))
+        self.image_layout = QVBoxLayout(self.verticalLayoutWidget)
+        self.image_layout.setObjectName(u"image_layout")
+        self.image_layout.setContentsMargins(0, 0, 0, 0)
+        self.add_img_btn = QPushButton(self.verticalLayoutWidget)
+        self.add_img_btn.setObjectName(u"add_img_btn")
+
+        self.image_layout.addWidget(self.add_img_btn)
+
+        self.save_as_btn = QPushButton(self.verticalLayoutWidget)
+        self.save_as_btn.setObjectName(u"save_as_btn")
+
+        self.image_layout.addWidget(self.save_as_btn)
+
+        self.save_btn = QPushButton(self.verticalLayoutWidget)
+        self.save_btn.setObjectName(u"save_btn")
+
+        self.image_layout.addWidget(self.save_btn)
+
+        self.Settings.addItem(self.page, u"Image")
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.page_2.setGeometry(QRect(0, 0, 229, 557))
+        self.verticalLayoutWidget_3 = QWidget(self.page_2)
+        self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
+        self.verticalLayoutWidget_3.setGeometry(QRect(20, 10, 181, 531))
+        self.settings_layout = QVBoxLayout(self.verticalLayoutWidget_3)
+        self.settings_layout.setObjectName(u"settings_layout")
+        self.settings_layout.setContentsMargins(0, 0, 0, 0)
+        self.groupBox = QGroupBox(self.verticalLayoutWidget_3)
+        self.groupBox.setObjectName(u"groupBox")
+        self.color_slider = QSlider(self.groupBox)
+        self.color_slider.setObjectName(u"color_slider")
+        self.color_slider.setGeometry(QRect(30, 60, 22, 121))
+        self.color_slider.setMaximum(100)
+        self.color_slider.setOrientation(Qt.Vertical)
+        self.light_slider = QSlider(self.groupBox)
+        self.light_slider.setObjectName(u"light_slider")
+        self.light_slider.setGeometry(QRect(80, 60, 22, 121))
+        self.light_slider.setMaximum(100)
+        self.light_slider.setOrientation(Qt.Vertical)
+        self.dark_slider = QSlider(self.groupBox)
+        self.dark_slider.setObjectName(u"dark_slider")
+        self.dark_slider.setGeometry(QRect(130, 60, 22, 121))
+        self.dark_slider.setMaximum(100)
+        self.dark_slider.setOrientation(Qt.Vertical)
+        self.color_spinbox = QSpinBox(self.groupBox)
+        self.color_spinbox.setObjectName(u"color_spinbox")
+        self.color_spinbox.setGeometry(QRect(20, 190, 42, 26))
+        self.light_spinbox = QSpinBox(self.groupBox)
+        self.light_spinbox.setObjectName(u"light_spinbox")
+        self.light_spinbox.setGeometry(QRect(70, 190, 42, 26))
+        self.dark_spinbox = QSpinBox(self.groupBox)
+        self.dark_spinbox.setObjectName(u"dark_spinbox")
+        self.dark_spinbox.setGeometry(QRect(120, 190, 42, 26))
+        self.color_label = QLabel(self.groupBox)
+        self.color_label.setObjectName(u"color_label")
+        self.color_label.setGeometry(QRect(20, 20, 41, 20))
+        self.color_label.setAlignment(Qt.AlignCenter)
+        self.light_label = QLabel(self.groupBox)
+        self.light_label.setObjectName(u"light_label")
+        self.light_label.setGeometry(QRect(70, 20, 41, 20))
+        self.light_label.setAlignment(Qt.AlignCenter)
+        self.dark_label = QLabel(self.groupBox)
+        self.dark_label.setObjectName(u"dark_label")
+        self.dark_label.setGeometry(QRect(120, 20, 41, 20))
+        self.dark_label.setAlignment(Qt.AlignCenter)
+        self.color_chbox = QCheckBox(self.groupBox)
+        self.color_chbox.setObjectName(u"color_chbox")
+        self.color_chbox.setEnabled(True)
+        self.color_chbox.setGeometry(QRect(33, 40, 16, 16))
+        self.color_chbox.setLayoutDirection(Qt.LeftToRight)
+        self.color_chbox.setAutoFillBackground(False)
+        self.light_chbox = QCheckBox(self.groupBox)
+        self.light_chbox.setObjectName(u"light_chbox")
+        self.light_chbox.setEnabled(True)
+        self.light_chbox.setGeometry(QRect(84, 40, 16, 16))
+        self.light_chbox.setLayoutDirection(Qt.LeftToRight)
+        self.light_chbox.setAutoFillBackground(False)
+        self.dark_chbox = QCheckBox(self.groupBox)
+        self.dark_chbox.setObjectName(u"dark_chbox")
+        self.dark_chbox.setEnabled(True)
+        self.dark_chbox.setGeometry(QRect(133, 40, 16, 16))
+        self.dark_chbox.setLayoutDirection(Qt.LeftToRight)
+        self.dark_chbox.setAutoFillBackground(False)
+        self.normal_radiobtn = QRadioButton(self.groupBox)
+        self.normal_radiobtn.setObjectName(u"normal_radiobtn")
+        self.normal_radiobtn.setGeometry(QRect(30, 230, 110, 24))
+        self.triangle_radiobtn = QRadioButton(self.groupBox)
+        self.triangle_radiobtn.setObjectName(u"triangle_radiobtn")
+        self.triangle_radiobtn.setGeometry(QRect(30, 250, 110, 24))
+        self.mono_radiobtn = QRadioButton(self.groupBox)
+        self.mono_radiobtn.setObjectName(u"mono_radiobtn")
+        self.mono_radiobtn.setGeometry(QRect(30, 290, 110, 24))
+        self.dual_radiobtn = QRadioButton(self.groupBox)
+        self.dual_radiobtn.setObjectName(u"dual_radiobtn")
+        self.dual_radiobtn.setGeometry(QRect(30, 270, 110, 24))
+        self.random_btn = QPushButton(self.groupBox)
+        self.random_btn.setObjectName(u"random_btn")
+        self.random_btn.setGeometry(QRect(90, 460, 81, 29))
+        self.save_this_btn = QPushButton(self.groupBox)
+        self.save_this_btn.setObjectName(u"save_this_btn")
+        self.save_this_btn.setGeometry(QRect(10, 460, 81, 29))
+
+        self.settings_layout.addWidget(self.groupBox)
+
+        self.apply_btn = QPushButton(self.verticalLayoutWidget_3)
+        self.apply_btn.setObjectName(u"apply_btn")
+
+        self.settings_layout.addWidget(self.apply_btn)
+
+        self.Settings.addItem(self.page_2, u"Options")
+
+        self.verticalLayout_3.addWidget(self.Settings)
+
+        self.image_shower = QLabel(self.centralwidget)
+        self.image_shower.setObjectName(u"image_shower")
+        self.image_shower.setGeometry(QRect(262, 19, 711, 611))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1001, 22))
+        self.menuPaintroom = QMenu(self.menubar)
+        self.menuPaintroom.setObjectName(u"menuPaintroom")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuPaintroom.menuAction())
+
+        self.retranslateUi(MainWindow)
+
+        self.Settings.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.add_img_btn.setText(QCoreApplication.translate("MainWindow", u"Add image", None))
+        self.save_as_btn.setText(QCoreApplication.translate("MainWindow", u"Save as", None))
+        self.save_btn.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.Settings.setItemText(self.Settings.indexOf(self.page), QCoreApplication.translate("MainWindow", u"Image", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Color", None))
+        self.color_label.setText(QCoreApplication.translate("MainWindow", u"Color", None))
+        self.light_label.setText(QCoreApplication.translate("MainWindow", u"Light", None))
+        self.dark_label.setText(QCoreApplication.translate("MainWindow", u"Dark", None))
+        self.color_chbox.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.light_chbox.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.dark_chbox.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        self.normal_radiobtn.setText(QCoreApplication.translate("MainWindow", u"Normal", None))
+        self.triangle_radiobtn.setText(QCoreApplication.translate("MainWindow", u"Triangle", None))
+        self.mono_radiobtn.setText(QCoreApplication.translate("MainWindow", u"Mono", None))
+        self.dual_radiobtn.setText(QCoreApplication.translate("MainWindow", u"Dual", None))
+        self.random_btn.setText(QCoreApplication.translate("MainWindow", u"Random", None))
+        self.save_this_btn.setText(QCoreApplication.translate("MainWindow", u"Save this", None))
+        self.apply_btn.setText(QCoreApplication.translate("MainWindow", u"Apply", None))
+        self.Settings.setItemText(self.Settings.indexOf(self.page_2), QCoreApplication.translate("MainWindow", u"Options", None))
+        self.image_shower.setText("")
+        self.menuPaintroom.setTitle(QCoreApplication.translate("MainWindow", u"Paintroom", None))
+    # retranslateUi
+
